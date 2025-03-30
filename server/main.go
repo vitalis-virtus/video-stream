0274b9e4449018d9c42c0e14590ce457c8e2fac1
vitalis_virtus <vshayuk@gmail.com>
2025-03-30 23:42:34 +0300

    Add cors

--- server/main.go
+++ server/main.go
@@ -18,11 +18,21 @@ func main() {
 	}
 
 	fs := http.FileServer(http.Dir(hlsDir))
-	http.Handle("/hls/", http.StripPrefix("/hls/", fs))
+	http.Handle("/hls/", http.StripPrefix("/hls/", addCors(fs)))
 
 	port := 8080
 
 	fmt.Printf("Starting HLS server on http://localhost:%d/hls/\n", port)
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
+
+func addCors(fs http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		(w).Header().Set("Access-Control-Allow-Origin", "*")
+		(w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		(w).Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
+
+		fs.ServeHTTP(w, r)
+	}
 }
