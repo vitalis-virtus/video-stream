dc8ada34adf0daa8dc80b056582cc480c104d209
vitalis_virtus <vshayuk@gmail.com>
2025-03-30 23:25:22 +0300

    Add server implementation

--- server/main.go
+++ server/main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	hlsDir := "./videos"
+
+	_, err := os.Stat(hlsDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatal("HLS Directory doesn't exist")
+		}
+	}
+
+	fs := http.FileServer(http.Dir(hlsDir))
+	http.Handle("/hls/", http.StripPrefix("/hls/", fs))
+
+	port := 8080
+
+	fmt.Printf("Starting HLS server on http://localhost:%d/hls/\n", port)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+}
